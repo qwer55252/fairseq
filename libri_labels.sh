448c681df455a208bedb9d863551b36c208f77a8
sangheon <qwer55252@gmail.com>
2024-10-08 10:47:06 +0900

    add: eval 데이터셋 전처리 스크립트 추가

--- libri_labels.sh
+++ libri_labels.sh
@@ -3,7 +3,12 @@
 # --output-dir /home/kobie/workspace/data/LibriSpeech \
 # --output-name train
 
+# python examples/wav2vec/libri_labels.py \
+# /home/kobie/workspace/data/LibriSpeech/manifest/valid.tsv \
+# --output-dir /home/kobie/workspace/data/LibriSpeech \
+# --output-name valid
+
 python examples/wav2vec/libri_labels.py \
-/home/kobie/workspace/data/LibriSpeech/manifest/valid.tsv \
---output-dir /home/kobie/workspace/data/LibriSpeech \
---output-name valid
+/home/kobie/workspace/data/LibriSpeech/test-clean/train.tsv \
+--output-dir /home/kobie/workspace/data/LibriSpeech/test-clean \
+--output-name test
