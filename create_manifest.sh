b3134bde2e3e722c96d8c6015bb17e39c99b4fcb
sangheon <qwer55252@gmail.com>
2024-10-08 10:46:24 +0900

    add: eval script 추가

--- create_manifest.sh
+++ create_manifest.sh
@@ -1,8 +1,14 @@
 ext=flac
-valid=0.01
+valid=0.00
 
 python examples/wav2vec/wav2vec_manifest.py \
 /home/kobie/workspace/data/LibriSpeech/train-clean-100 \
 --dest /home/kobie/workspace/data/LibriSpeech/manifest \
+--ext $ext \
+--valid-percent $valid
+
+python examples/wav2vec/wav2vec_manifest.py \
+/home/kobie/workspace/data/LibriSpeech/test-clean \
+--dest /home/kobie/workspace/data/LibriSpeech/test-clean \
 --ext $ext \
 --valid-percent $valid
